a3926acb13df583280a2070c6369524b2e1992b3
cesarlarah4 <cesarlarah4@gmail.com>
2025-12-11 11:03:06 -0600

    alb change

--- main.tf
+++ main.tf
@@ -46,40 +46,41 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
+  source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+  subnets = [module.blog_sg.public_subnets]
 
-  security_groups = [module.blog_sg.security_group_id]
-
-  target_groups = [
-    {
-   
-      name_prefix = "blog-"
-      backend_protocol  = "HTTP"
-      backend_port  = 80
-
-      my_targets ={
-        target_type  = "instance"
-        target_id  = aws_instance.blog.id
-        port  = 80
-        }      
-    }
-  ]
-
-  listeners = [
-    {
-      port  = 80
-      protocol  = "HTTP"
-      target_group_index  = 0
-    
+  # Security Group
+  security_group_ingress_rules = [module.blog_sg.security_group_id]
+  security_group_egress_rules = [module.blog_sg.security_group_id]
+  
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }   
   }
-] 
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
 
   tags = {
-    Environment = "Dev"    
+    Environment = "Dev"
+    Project     = "Example"
   }
 }
 
