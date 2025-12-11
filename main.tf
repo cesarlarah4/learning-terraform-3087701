c117895e5c9cc5a2c87373a55a73094de278e033
cesarlarah4 <cesarlarah4@gmail.com>
2025-12-11 11:06:30 -0600

    security groups fix

--- main.tf
+++ main.tf
@@ -53,8 +53,7 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
 
   # Security Group
-  security_group_ingress_rules = [module.blog_sg.security_group_id]
-  security_group_egress_rules = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]  
   
   listeners = {
     ex-http-https-redirect = {
